d909ceb1dce441c63bef657bbc834df6031f3d63
Tojo Jose <tojopjose@Tojos-MBP.fios-router.home>
2016-02-26 17:24:24 -0500

    adding project files

---
+++ src/com/aws/web/servlets/ContactServlet.java
@@ -0,0 +1,122 @@
+package com.aws.web.servlets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+
+
+
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+
+
+
+import com.aws.web.util.MailAccountInfo;
+import com.aws.web.util.SnsSender;
+
+public class ContactServlet extends HttpServlet {
+    /**
+	 * 
+	 */
+    private static final long serialVersionUID = 1L;
+    MailAccountInfo mailInfo = new MailAccountInfo();
+    String sTo = "jgeorge@acr.org",
+	    sContent = "",
+	    sSubject = "Contact";
+
+    /**
+     * Constructor of the object.
+     */
+    public ContactServlet() {
+	super();
+    }
+
+    /**
+     * Destruction of the servlet. <br>
+     */
+    @Override
+    public void destroy() {
+	super.destroy();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+
+   
+    	
+	process(request, response);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+	   process(request, response);
+    }
+
+    private void process(HttpServletRequest request,
+	    HttpServletResponse response) throws IOException {
+ 
+     String payload = getBody(request);   
+     System.out.println(payload);
+     SnsSender  snsSender = new SnsSender();
+     snsSender.sendToArn(payload);
+     
+    }
+    
+
+    public void writeToAwsSNS(String sFirstName, String sLastName, String sEmail, String sPhoneNo, String sCompany) {
+	    sContent = " Name: " + sFirstName + " " + sLastName + " \n " + 
+	    		" Email: " + sEmail + " \n " + " Phone: " + sPhoneNo + " \n " + " Company: " + sCompany; 
+	    System.out.println("-------a----");		
+	    SnsSender  snsSender = new SnsSender();
+	    snsSender.sendToArn(sContent);
+	    System.out.println("-------b----");
+    }
+    
+    public static String getBody(HttpServletRequest request) throws IOException {
+
+        String body = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+
+        try {
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                char[] charBuffer = new char[128];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            } else {
+                stringBuilder.append("");
+            }
+        } catch (IOException ex) {
+            throw ex;
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException ex) {
+                    throw ex;
+                }
+            }
+        }
+
+        body = stringBuilder.toString();
+        return body;
+    }
+    
+    
+}
---
+++ src/com/aws/web/util/SnsSender.java
@@ -0,0 +1,35 @@
+package com.aws.web.util;
+
+import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
+
+
+public class SnsSender {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a test");
+		
+		SnsSender sns = new SnsSender();
+		sns.sendToArn("test");
+	}
+	
+	public void sendToArn(String message){
+		String topicArn = "###########";
+		AmazonSNSClient snsClient = new AmazonSNSClient();		          
+		snsClient.withRegion(Regions.US_EAST_1);
+	
+		
+		//publish to an SNS topic
+		String msg = message;
+		PublishRequest publishRequest = new PublishRequest(topicArn, msg);
+		PublishResult publishResult = snsClient.publish(publishRequest);
+		//print MessageId of message published to SNS topic
+		System.out.println("MessageId - " + publishResult.getMessageId());
+	}
+	
+	
+}
